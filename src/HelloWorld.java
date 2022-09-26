8e4587863448ff2e94428bb5bddc3ce3ebb237d2
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2022-09-26 20:50:07 +0700

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        byte ukuran = 10;
+        short pendekNi = 250;
+
+        System.out.println(ukuran);
+        System.out.println(pendekNi);
+    }
+}
